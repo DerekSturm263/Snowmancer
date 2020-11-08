bcb41c41e9db6aa532e0c454b1e8c95e3a147791
Derek Sturm <dereksturm263@gmail.com>
2020-11-07 17:09:17 -0800

    Added my third person movement system. It's not finished as you can tell, but I'm almost done I think. The camera is a little weird right now too but I will fix that later.

---
+++ Assets/Resources/Scripts/IKController.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+
+public class IKController : MonoBehaviour
+{
+    private Animator anim;
+
+    [Header("Foot IK Settings")]
+
+    public GameObject lFoot;
+    public GameObject rFoot;
+    public GameObject lKnee;
+    public GameObject rKnee;
+
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float groundRaycastLength;
+    [SerializeField] private float slopeRaycastLength;
+
+    public float lFootWeight;
+    public float rFootWeight;
+
+    private Transform lFootTarget, lKneeTarget;
+    private Transform rFootTarget, rKneeTarget;
+
+    [Header("Debug Mesh Settings")]
+    public Mesh renderMesh;
+    public Material renderMat;
+    public bool visualize;
+
+    public float kneeValueL, kneeValueR;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+
+        lFootTarget = BuildTarget("Left Foot Target");
+        rKneeTarget = BuildTarget("Left Knee Target");
+
+        rFootTarget = BuildTarget("Right Foot Target");
+        lKneeTarget = BuildTarget("Right Knee Target");
+    }
+
+    /// <summary>
+    /// Returns the transform of a new GameObject with the given name.
+    /// </summary>
+    /// <param name="name">Name of the new IK target.</param>
+    /// <returns></returns>
+    private Transform BuildTarget(string name, bool finalTarget = false)
+    {
+        GameObject g = new GameObject(name);
+
+        if (visualize) g.AddComponent<MeshFilter>().mesh = renderMesh;
+        if (visualize) g.AddComponent<MeshRenderer>().material = renderMat;
+        g.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+
+        return g.transform;
+    }
+
+    /// <summary>
+    /// Sends out a raycast from each foot with information about position and normals.
+    /// </summary>
+    private void SetFootRaycasts()
+    {
+        // Left Foot Transform.
+        if (Physics.Raycast(lFoot.transform.position, Vector3.down, out RaycastHit lFootHit, groundRaycastLength, ground, QueryTriggerInteraction.UseGlobal)
+            && lFootTarget.gameObject.activeSelf)
+        {
+            lFootTarget.position = lFootHit.point;
+            lFootTarget.transform.up = lFootHit.normal;
+        }
+ 
+        // Right Foot Transform.
+        if (Physics.Raycast(rFoot.transform.position, Vector3.down, out RaycastHit rFootHit, groundRaycastLength, ground, QueryTriggerInteraction.UseGlobal)
+            && rFootTarget.gameObject.activeSelf)
+        {
+            rFootTarget.position = rFootHit.point;
+            rFootTarget.transform.up = rFootHit.normal;
+        }
+    }
+
+    private void SetKneeRaycasts()
+    {
+        // Left Knee Transform.
+        if (Physics.Raycast(lKnee.transform.position, Vector3.down + transform.forward, out RaycastHit lKneeHit, slopeRaycastLength, ground, QueryTriggerInteraction.UseGlobal)
+            && lKneeTarget.gameObject.activeSelf && anim.GetFloat("LFootLerp") != 0f)
+        {
+            lKneeTarget.position = lKneeHit.point + lKneeHit.normal * 0.8f;
+            lKneeTarget.transform.up = lKneeHit.normal;
+        }
+
+        // Right Knee Transform.
+        if (Physics.Raycast(rKnee.transform.position, Vector3.down + transform.forward, out RaycastHit rKneeHit, slopeRaycastLength, ground, QueryTriggerInteraction.UseGlobal)
+            && rKneeTarget.gameObject.activeSelf && anim.GetFloat("RFootLerp") != 0f)
+        {
+            rKneeTarget.position = rKneeHit.point + rKneeHit.normal * 0.8f;
+            rKneeTarget.transform.up = rKneeHit.normal;
+        }
+    }
+
+    /// <summary>
+    /// Adjusts the animation IK position and rotation, as well as their respective weights.
+    /// </summary>
+    private void SetIKs()
+    {
+        // Set weights.
+        if (anim.GetFloat("LFootLerp") > 0.01f)
+        {
+            anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot, anim.GetFloat("LFootLerp"));
+            anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, anim.GetFloat("LFootLerp"));
+        }
+        else
+        {
+            anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1f);
+            anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1f);
+        }
+        if (anim.GetFloat("RFootLerp") > 0.01f)
+        {
+            anim.SetIKPositionWeight(AvatarIKGoal.RightFoot, anim.GetFloat("RFootLerp"));
+            anim.SetIKRotationWeight(AvatarIKGoal.RightFoot, anim.GetFloat("RFootLerp"));
+        }
+        else
+        {
+            anim.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1f);
+            anim.SetIKRotationWeight(AvatarIKGoal.RightFoot, 1f);
+        }
+
+        //if (anim.GetFloat("LKneeLerp") > 0.01f)
+            anim.SetIKHintPositionWeight(AvatarIKHint.LeftKnee, 1f);
+        //else
+            //anim.SetIKHintPositionWeight(AvatarIKHint.LeftKnee, 0f);
+
+        //if (anim.GetFloat("RKneeLerp") > 0.01f)
+            anim.SetIKHintPositionWeight(AvatarIKHint.RightKnee, 1f);
+        //else
+            //anim.SetIKHintPositionWeight(AvatarIKHint.RightKnee, 0f);
+
+
+
+        // Set positions.
+        anim.SetIKPosition(AvatarIKGoal.LeftFoot, lFootTarget.position);
+        anim.SetIKRotation(AvatarIKGoal.LeftFoot, lFootTarget.rotation * transform.rotation);
+        anim.SetIKPosition(AvatarIKGoal.RightFoot, rFootTarget.position);
+        anim.SetIKRotation(AvatarIKGoal.RightFoot, rFootTarget.rotation * transform.rotation);
+
+        anim.SetIKHintPosition(AvatarIKHint.LeftKnee, lKneeTarget.position);
+        anim.SetIKHintPosition(AvatarIKHint.RightKnee, rKneeTarget.position);
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        SetFootRaycasts();
+        SetKneeRaycasts();
+        SetIKs();
+
+        Debug.DrawRay(lFoot.transform.position, Vector3.down * groundRaycastLength, Color.red, 0);
+        Debug.DrawRay(rFoot.transform.position, Vector3.down * groundRaycastLength, Color.blue, 0);
+        Debug.DrawRay(lKnee.transform.position, (Vector3.down + transform.forward) * slopeRaycastLength, Color.red, 0);
+        Debug.DrawRay(rKnee.transform.position, (Vector3.down + transform.forward) * slopeRaycastLength, Color.blue, 0);
+    }
+}
---
+++ Assets/Resources/Scripts/Movement.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Animator anim;
+    private ParticleSystem ps;
+
+    public Camera cam;
+
+    private Vector3 movementVector;
+    private Vector3 targetVector;
+    private bool isRunning;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        ps = GetComponent<ParticleSystem>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Move(Vector2 amount)
+    {
+        targetVector = Vector3.zero;
+        float multiplier = isRunning ? 2f : 1f;
+
+        targetVector += amount.x * cam.transform.right * multiplier;
+        targetVector += amount.y * cam.transform.forward * multiplier;
+
+        ps.Play();
+    }
+
+    public void Run(bool run)
+    {
+        isRunning = run;
+    }
+
+    public void Jump()
+    {
+        anim.SetTrigger("Jump");
+    }
+
+    public void Attack()
+    {
+        anim.SetTrigger("Attack");
+    }
+
+    private void Update()
+    {
+        Move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+        Run(Input.GetButton("Run"));
+
+        movementVector = Vector3.Lerp(movementVector, targetVector, Time.deltaTime * 10f);
+
+        if (targetVector != Vector3.zero)
+            transform.rotation = Quaternion.LookRotation(movementVector, Vector3.up);
+        else
+            ps.Stop();
+
+        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+        anim.SetFloat("Vertical", movementVector.magnitude);
+    }
+}
