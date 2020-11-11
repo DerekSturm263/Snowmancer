a5ac0ea702bf69f8c29688ff6cf6c4cd68d71fc7
Derek Sturm <dereksturm263@gmail.com>
2020-11-11 10:50:27 -0800

    Made some small adjustments.

--- Assets/Resources/Scripts/Movement.cs
+++ Assets/Resources/Scripts/Movement.cs
@@ -2,67 +2,51 @@
 
 public class Movement : MonoBehaviour
 {
-    private Animator anim;
-    private ParticleSystem ps;
+    protected Animator anim;
+    protected ParticleSystem ps;
 
     public Camera cam;
     public LayerMask ground;
 
-    private Vector3 movementVector;
-    private Vector3 targetVector;
-    private bool isRunning;
+    protected Vector3 movementVector;
+    protected Vector3 targetVector;
+    protected bool isRunning;
 
-    private void Awake()
+    protected void Awake()
     {
         anim = GetComponent<Animator>();
         ps = GetComponent<ParticleSystem>();
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    public void Move(Vector2 move)
+    protected void Move(Vector2 move)
     {
+        if (move == Vector2.zero)
+        {
+            if (ps.isPlaying) ps.Stop();
+            return;
+        }
+        ps.Play();
+
         targetVector = Vector3.zero;
         float multiplier = isRunning ? 2f : 1f;
 
         targetVector += move.x * cam.transform.right * multiplier;
         targetVector += move.y * cam.transform.forward * multiplier;
-
-        ps.Play();
     }
 
-    public void Run(bool run)
+    protected void Run(bool run)
     {
         isRunning = run;
     }
 
-    public void Jump()
+    protected void Jump()
     {
         anim.SetTrigger("Jump");
     }
 
-    public void Attack()
-    {
-        anim.SetTrigger("Attack");
-    }
-
-    private void Update()
-    {
-        Move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
-        Run(Input.GetButton("Run"));
-        Cursor.lockState = CursorLockMode.Locked;
-
-        movementVector = Vector3.Lerp(movementVector, targetVector, Time.deltaTime * 10f);
-
-        if (targetVector != Vector3.zero)
-            transform.rotation = Quaternion.LookRotation(movementVector, Vector3.up);
-        else
-            ps.Stop();
-
-        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-
-        anim.SetFloat("Vertical", movementVector.magnitude);
-    }
-
-    private void LateUpdate()
+    protected void LateUpdate()
     {
         if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, ground) && anim.velocity.magnitude > 0.1f)
         {
