f0bdb98699d516a33b08a6e0b6df2f8a588b15ed
JaredNo <jar091002@yahoo.com>
2020-11-22 20:54:30 -0800

    Pause menu with animation

--- Assets/Resources/Scripts/UI/UIController.cs
+++ Assets/Resources/Scripts/UI/UIController.cs
@@ -2,30 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
     //public Slider healthSlider;
     //public Slider manaSlider;
+
     public GameObject spellSelector;
     private int spellID;
     private GameObject[] menuItems;
     private MenuItemScript menuItemSc;
 
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
     void Update()
     {
-        if (Input.GetKey(KeyCode.Tab) || Input.GetKey(KeyCode.E))
+        if (!isPaused)
         {
-            spellSelector.SetActive(true);
-            menuItems = spellSelector.GetComponent<MenuScript>().menuItems;
-            spellID = spellSelector.GetComponent<MenuScript>().spellID;
-            menuItemSc = menuItems[spellID].GetComponent<MenuItemScript>();
+            if (Input.GetKey(KeyCode.Tab) || Input.GetKey(KeyCode.E))
+            {
+                spellSelector.SetActive(true);
+                menuItems = spellSelector.GetComponent<MenuScript>().menuItems;
+                spellID = spellSelector.GetComponent<MenuScript>().spellID;
+                menuItemSc = menuItems[spellID].GetComponent<MenuItemScript>();
 
+            }
+            else if (Input.GetKeyUp(KeyCode.Tab) || Input.GetKeyUp(KeyCode.E))
+            {
+                menuItemSc.SetCurrentSpell();
+                spellSelector.SetActive(false);
+            }
         }
-        else if (Input.GetKeyUp(KeyCode.Tab) || Input.GetKeyUp(KeyCode.E))
+
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            menuItemSc.SetCurrentSpell();
-            spellSelector.SetActive(false);
+            if (isPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
         }
 
     }
@@ -53,4 +72,37 @@ public class UIController : MonoBehaviour
     //{
     //    manaSlider.value = mana;
     //}
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        spellSelector.SetActive(false);
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Load menu");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Title");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit game");
+        Application.Quit();
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Settings open");
+    }
 }
