5a1863347fb2172daf6bb73469841c62c129d078
Derek Sturm <dereksturm263@gmail.com>
2020-12-10 19:20:07 -0800

    Added cloth simulation to the racoon tail.
    I will continue to tweak it until it looks good and then add cloth simulation to the enemy hats and player backpack.

---
+++ Assets/Resources/Scripts/UpdateAcceleration.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+
+public class UpdateAcceleration : MonoBehaviour
+{
+    private Cloth cloth;
+    private VisualEffect wind;
+    private Skybox mainSkybox;
+
+    private void Awake()
+    {
+        cloth = GetComponent<Cloth>();
+        wind = FindObjectOfType<VisualEffect>();
+    }
+
+    private void Update()
+    {
+        Vector3 windDir = wind.GetVector3("Wind");
+
+        cloth.externalAcceleration = windDir;
+        RenderSettings.skybox.SetFloat("_Speed", windDir.magnitude / 600f);
+    }
+}
