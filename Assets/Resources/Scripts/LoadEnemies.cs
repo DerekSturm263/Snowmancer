aa3baec0c6f848a09ef7551acf86fcf8a64f01e4
Derek Sturm <dereksturm263@gmail.com>
2021-01-04 11:16:20 -0800

    Added Enemy Loading Prefab

---
+++ Assets/Resources/Scripts/LoadEnemies.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadEnemies : MonoBehaviour
+{
+    public List<GameObject> enemies = new List<GameObject>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        enemies.ForEach(x => x.SetActive(true));
+        gameObject.SetActive(false);
+    }
+}
