fd3ffca868783008b8ef5d7cc857b8d8acf2db61
Derek Sturm <dereksturm263@gmail.com>
2021-01-14 02:03:11 -0800

    Fixed something.

--- Assets/Resources/Scripts/EnterNextLevel.cs
+++ Assets/Resources/Scripts/EnterNextLevel.cs
@@ -3,9 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class EnterNextLevel : MonoBehaviour
 {
+    public bool active;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && active)
         {
             SceneManager.LoadScene("Level 2");
         }
