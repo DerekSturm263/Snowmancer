f209c17ed290dee4d3eb2b62edbb0ee2969518d9
Snow47 <Aidangamerguy@gmail.com>
2021-01-17 00:12:10 -0800

    Saving/Loading
    added several data types for enemies and player

---
+++ Assets/Resources/Scripts/Save games/PlayerData.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    //player
+    public float maxHealth;
+    public float health;
+    public float maxMana;
+    public float mana;
+    public float manaRegenSpeed;
+    public float maxSnow;
+    public float snowAmount;
+    public bool aiming;
+    public bool selectingSpell;
+    public Player.CurrentSpell currentSpell;
+    public float[] targetPos;
+    public float[] postition;
+   
+
+    public PlayerData(Player player)
+    {
+        //player
+        maxHealth = player.maxHealth;
+        health = player.health;
+        maxMana = player.maxMana;
+        mana = player.mana;
+        maxSnow = player.maxSnow;
+        snowAmount = player.snowAmount;
+        aiming = player.aiming;
+        selectingSpell = player.selectingSpell;
+        currentSpell = player.currentSpell;
+       
+
+        //player Postition
+        targetPos = new float[3];
+        targetPos[0] = player.transform.position.x;
+        targetPos[1] = player.transform.position.y;
+        targetPos[2] = player.transform.position.z;
+
+        postition = new float[3];
+        postition[0] = player.transform.position.x;
+        postition[1] = player.transform.position.y;
+        postition[2] = player.transform.position.z;
+
+    }
+
+}
---
+++ Assets/Resources/Scripts/Save games/SaveLoad.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SaveLoad : MonoBehaviour
+{
+    private List <Enemy> enemy = new List<Enemy>();
+    private MenuItemScript spellIcon;
+    private Player player;
+    private void Awake()
+    {
+        player = GameObject.FindObjectOfType<Player>();
+        spellIcon = GameObject.FindObjectOfType<MenuItemScript>();
+        enemy = GameObject.FindObjectsOfType<Enemy>().ToList();
+    }
+    //Save/load system stuff
+    public void SavePlayer()
+    {
+        Debug.Log("saved!");
+        SaveSystem.SavePlayer(player);
+        foreach (Enemy e in enemy)
+        {
+            SaveSystem.SaveEnemy(e);
+        }
+    }
+
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveSystem.LoadPLayer();
+        EnemyData eData = SaveSystem.LoadEnemy();
+        Debug.Log("loading...");
+        
+        //player Info
+        player.maxHealth = data.maxHealth;
+        player.health = data.health;
+        player.maxMana = data.maxMana;
+        player.mana = data.mana;
+        player.maxSnow = data.maxSnow;
+        player.snowAmount = data.snowAmount;
+        player.aiming = data.aiming;
+        player.selectingSpell = data.selectingSpell;
+        player.currentSpell = data.currentSpell;
+        //Enemies Info
+        foreach (Enemy e in enemy)
+        {
+            e.maxHealth = eData.eMaxHealth;
+            e.health = eData.eHealth;
+
+            //postition
+            Vector3 ePostition;
+            ePostition.x = eData.ePostition[0];
+            ePostition.y = eData.ePostition[1];
+            ePostition.z = eData.ePostition[2];
+            e.transform.position = ePostition;
+        }
+
+        //Player postition
+        //change to teliport to postition not glide 
+        Vector3 targetPos;
+        targetPos.x = data.postition[0];
+        targetPos.y = data.postition[1];
+        targetPos.z = data.postition[2];
+        player.GetComponent<PlayerMovement>().targetPos = targetPos;
+
+        Vector3 postition;
+        postition.x = data.postition[0];
+        postition.y = data.postition[1];
+        postition.z = data.postition[2];
+        transform.position = postition;
+
+
+        //Spell icon change
+        spellIcon.nameDisplay.text = spellIcon.runeName;
+        spellIcon.background.color = spellIcon.hoverColor;
+        spellIcon.currentSpell.sprite = spellIcon.icon;
+
+    }
+}
